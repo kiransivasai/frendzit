8a2bb88278d6c713f7eba110679fa9c5ff0dfb31
kiransivasai <kiransiva2060@gmail.com>
2021-07-11 11:17:48 +0530

    Updated files

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,14 @@ import "firebase/firestore";
 import "firebase/database";
 
 const firebaseApp = firebase.initializeApp({
-  // Add firebase config here
+  apiKey: "AIzaSyAilauIj9O597BY4S6BI8bfP5z3LC4Pu90",
+  authDomain: "frendzit-a0ec6.firebaseapp.com",
+  databaseURL: "https://frendzit-a0ec6.firebaseio.com",
+  projectId: "frendzit-a0ec6",
+  storageBucket: "frendzit-a0ec6.appspot.com",
+  messagingSenderId: "645527780923",
+  appId: "1:645527780923:web:a38d5d455533bf55b2661e",
+  measurementId: "G-M4SMH95PJZ",
 });
 const db = firebaseApp.firestore();
 const storage = firebase.storage();
